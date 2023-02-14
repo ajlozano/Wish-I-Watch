f7eeed72f1ce6d5035abe524db97285146262495
Toni <ajlozano@users.noreply.github.com>
2023-02-14 16:40:21 +0100

    Add logo icon and view Controllers

---
+++ Wish-I-Watch/Controllers/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Wish-I-Watch
+//
+//  Created by Toni Lozano Fernández on 14/2/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Wish-I-Watch/K.swift
@@ -0,0 +1,12 @@
+//
+//  K.swift
+//  Wish-I-Watch
+//
+//  Created by Toni Lozano Fernández on 13/2/23.
+//
+
+import Foundation
+
+struct K {
+    static let titleName = "Wish I Watch"
+}
